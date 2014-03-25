227440bd9dff1bccb090054f0684c23edc10fa79
rpm <ryanmurray@live.ca>
2014-03-25 17:14:19 -0400

    changed host table name to radiohost, because of keyword.

--- BuildTables.sql
+++ BuildTables.sql
@@ -7,19 +7,19 @@
 -- Only artists can "perform" songs, not bands (at least not directly)
 
 -- Uncomment to reset tables:
--- drop table project.host cascade;
--- drop table project.show cascade;
--- drop table project.hostshow cascade;
--- drop table project.playsheet cascade;
--- drop table project.timeslot cascade;
--- drop table project.guest cascade;
--- drop table project.band cascade;
--- drop table project.artist cascade;
--- drop table project.album cascade;
--- drop table project.song cascade;
--- drop table project.track cascade;
+-- drop table project.radiohost;
+-- drop table project.show;
+-- drop table project.hostshow;
+-- drop table project.playsheet;
+-- drop table project.timeslot;
+-- drop table project.guest;
+-- drop table project.band;
+-- drop table project.artist;
+-- drop table project.album;
+-- drop table project.song;
+-- drop table project.track;
 
-create table project.host (
+create table project.radiohost (
 emp_num integer primary key,
 first_name text not null,
 last_name text not null,
@@ -37,8 +37,8 @@ category text );
 
 create table project.hostshow (
 contract_num integer primary key,
-emp_num integer references host(emp_num) on delete restrict,
-show_num integer references show(show_num) on delete restrict,
+emp_num integer references project.radiohost(emp_num) on delete restrict,
+show_num integer references project.show(show_num) on delete restrict,
 host_start_year integer,
 host_start_month text check (host_start_month in('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')) );
 
@@ -51,8 +51,8 @@ create table project.timeslot (
 slot_num integer primary key,
 start_time integer not null check (start_time >= 0),
 end_time integer not null check(end_time >= 0),
-show_num integer references show(show_num) on delete restrict,
-sheet_num integer references playsheet(sheet_num) on delete restrict );
+show_num integer references project.show(show_num) on delete restrict,
+sheet_num integer references project.playsheet(sheet_num) on delete restrict );
 
 create table project.guest (
 guest_num integer primary key,
@@ -61,7 +61,7 @@ last_name text not null,
 descr text,
 topic text,
 rating integer default 0 check (rating >= 0),
-slot_num integer references timeslot(slot_num) on delete restrict );
+slot_num integer references project.timeslot(slot_num) on delete restrict );
 
 create table project.band (
 band_num integer primary key,
@@ -71,7 +71,7 @@ nationality text );
 
 create table project.artist (
 artist_num integer primary key,
-band_num integer references band(band_num),
+band_num integer references project.band(band_num),
 first_name text not null,
 last_name text,
 stage_name text,
@@ -89,8 +89,8 @@ song_num integer primary key,
 title text not null,
 cancon boolean default false,
 instrumental boolean default false,
-album_num integer references album(album_num) on delete restrict,
-artist_num integer references artist(artist_num) on delete restrict );
+album_num integer references project.album(album_num) on delete restrict,
+artist_num integer references project.artist(artist_num) on delete restrict );
 
 create table project.track (
 track_num integer primary key,
@@ -98,4 +98,4 @@ title text,
 start_time integer check (start_time >= 0),
 end_time integer check (end_time >= 0),
 track_type text,
-song_num integer references song(song_num) on delete restrict );
+song_num integer references project.song(song_num) on delete restrict );
